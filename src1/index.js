0b285b58aa56cd7673d61769fd866ce52898d315
Vinod Akshat <pumkininriver@gmail.com>
2024-09-30 02:26:42 +0530

    added a mongo connection for no memory leakage

--- src1/index.js
+++ src1/index.js
@@ -7,6 +7,8 @@ const wer = require('./routes/request');
 require('./strategies/google');
 require('./strategies/spotify');
 const cors = require("cors");
+const mongoose = require('mongoose');
+const MongoStore = require('connect-mongo');
 
 passport.serializeUser((user, done) => {
     // Serialize the entire user object into the session
@@ -21,19 +23,34 @@ passport.deserializeUser((user, done) => {
 // 
     const app = express();
     app.use(express.json());
-    const PORT = process.env.PORT;
+
+
+    // Connect to MongoDB
+    mongoose.connect(process.env.MONGODB_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => console.log("MongoDB connected"))
+    .catch(err => console.log(err));
+    //const PORT = process.env.PORT;
+
     app.use(cors({
         origin: 'https://playlist-migration.vercel.app', 
         credentials: true 
     }));
-    app.use(
-        session({
-            secret: 'your-secret-key', 
-            resave: false, 
-            saveUninitialized: true, 
-            cookie: { secure: true }, 
-        })
-    );
+    app.use(session({
+        secret: 'your-secret-key', 
+        resave: false,
+        saveUninitialized: true,
+        store: MongoStore.create({
+            mongoUrl: process.env.MONGODB_URL,
+            collectionName: 'sessions', 
+        }),
+        cookie: {
+            secure: true, // Set to true if using HTTPS
+            maxAge: 3600000, // Session expiration time (1 hour)
+        },
+    }));
     app.use(passport.initialize());
     app.use(passport.session());
     app.get('/',(req,res)=>{
